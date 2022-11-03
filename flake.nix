4b87c6fde298fe32a6ec04e3879a7fb6bb1768ed
Trouble-Truffle <perigordtruffle7318@gmail.com>
2022-11-03 17:26:51 +0800

    Fixed non-existent input
    Fixed non-existent input
    
    Fixed non-existent input

--- flake.nix
+++ flake.nix
@@ -2,29 +2,23 @@
   description = "A bunch of overlays for Idris2";
 
   inputs = {
-    nixpkgs.follows = "nixpkgs-unstable";
-    flake-utils = { url = "github:numtide/flake-utils"; };
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ] (system:
-    let 
-      overlays = import ./overlays;
-      pkgs = import nixpkgs { overlays = [overlays]; };
-      in rec {
-        overlay = overlays;
-        devShells = with pkgs; {
-          default = mkShell {
-            buildInputs = [
-              idris2-nightly
-              idris2-lsp
-            ];
-          };
+    flake-utils.lib.eachDefaultSystem  (system:
+      let
+        overlays = import ./overlays;
+        pkgs = import nixpkgs { inherit system; overlays = [ overlays ]; };
+      in
+      {
+        overlays = overlays;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.idris2-nightly
+            pkgs.idris2-lsp
+          ];
         };
       });
 }
