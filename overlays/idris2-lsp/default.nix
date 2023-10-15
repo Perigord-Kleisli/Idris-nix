c9a4efbee25bdc0ce718c0f64aaf625591120df9
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-10-15 08:04:49 +0800

    chore: Update idris2-lsp

--- overlays/idris2-lsp/default.nix
+++ overlays/idris2-lsp/default.nix
@@ -2,7 +2,6 @@
   idris2-nightly,
   stdenv,
   fetchFromGitHub,
-  makeWrapper,
   lib,
 }:
 stdenv.mkDerivation {
@@ -14,8 +13,8 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "idris-community";
     repo = "idris2-lsp";
-    rev = "500f4a989c9583678230bf28a9f7b6a79f6be618";
-    sha256 = "DKWJi+7F8HSvcTRhjcYbYIf+N/NT7n2KVns/ies9KsE=";
+    rev = "7fa662a2ae416898152d7423a92cb1584a476304";
+    hash = "sha256-TqagaSmKR1WMG4gwVEpsAI9pRVezaS4zdWr4PEtwmnk=";
   };
 
   makeFlags = ["PREFIX=$(out)" "IDRIS2_PREFIX=$(out)" "IDRIS2_PACKAGE_PATH=$(out)"];
